7967b2571513922f511d65357bf18a641a8a05de
Faruuq <faruuqq@icloud.com>
2023-05-22 21:01:58 +0700

    add new files

---
+++ RatingControl/RatingControl.h
@@ -0,0 +1,18 @@
+//
+//  RatingControl.h
+//  RatingControl
+//
+//  Created by Muhammad Faruuq Qayyum on 22/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RatingControl.
+FOUNDATION_EXPORT double RatingControlVersionNumber;
+
+//! Project version string for RatingControl.
+FOUNDATION_EXPORT const unsigned char RatingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingControl/PublicHeader.h>
+
+
